be7ca68f05a2fff3584262c456e37d5db9011c8b
George Rodgers <george.rodgers.chem@gmail.com>
2024-10-15 15:50:17 +0100

    Express Restaurants Part 3 complete

--- src/app.js
+++ src/app.js
@@ -21,27 +21,31 @@ app.get(`/restaurants`, async (req, res) => {
 
 app.get(`/restaurants/:id`, async (req, res) => {
     const id = req.params.id;
-    const foundRestaurent = await Restaurant.findByPk(id);
-    res.json(foundRestaurent);
+    const foundRestaurant = await Restaurant.findByPk(id);
+    res.json(foundRestaurant);
 });
 
 // Code for Express Restaurant Part 3
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded());
 
-app.post(`/restaurants`, async (req, res, next) => {
-
-    try{
+app.post(`/restaurants`, async (req, res) => {
         const newRestaurant = await Restaurant.create(req.body);
-        if(newRestaurant.name == null){
-            throw new Error(`Resturant not created`)
-        }
-        res.send(newRestaurant)
-    }
-    catch(error){
-        next(error)
-    }
+        res.json(newRestaurant);
+});
+
+app.put(`/restaurants/:id`, async (req, res) => {
+    await Restaurant.update(req.body, {where: {id: req.params.id}});
+    const foundRestaurant = await Restaurant.findByPk(req.params.id);
+    res.json(foundRestaurant)
+})
+
+app.delete(`/restaurants/:id`, async (req, res) => {
+    const foundRestaurant = await Restaurant.findByPk(req.params.id);
+    await Restaurant.destroy({where: {id: req.params.id}});
+    // await foundRestaurant.destroy();
+    res.json(foundRestaurant)
 })
 
 module.exports = app;
